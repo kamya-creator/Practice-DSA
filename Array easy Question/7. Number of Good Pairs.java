bbb1f30d8fd552f365c8bf5db21f27e8a95c103a
kamya-creator <71540845+kamya-creator@users.noreply.github.com>
2022-01-23 20:58:18 +0530

    Create 7. Number of Good Pairs.java

---
+++ Array easy Question/7. Number of Good Pairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count =0;
+        int n =nums.length;
+        for(int i =0;i<n;i++)
+        {
+            for(int j = i+1;j<n;j++)
+            {
+                if(nums[i] == nums[j])
+                    count++;
+            }
+        } return count;
+    }
+}
