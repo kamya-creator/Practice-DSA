b44dfe57044e4db38325485790129c290aa20762
kamya-creator <71540845+kamya-creator@users.noreply.github.com>
2022-01-23 20:38:55 +0530

    Create 2. Concatenation of Array.java

---
+++ Array easy Question/2. Concatenation of Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[2*n];
+        
+        for(int i =0;i<n;i++)
+        {
+            res[i] = nums[i];
+            res[i+n] = nums[i];
+        }
+        return res;
+    }
+}
