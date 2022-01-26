1ab8de4971cd2de3dcce15f98d6947a90d48c871
kamya-creator <71540845+kamya-creator@users.noreply.github.com>
2022-01-26 18:06:28 +0530

    Create Find in Mountain.java

---
+++ Binary Search/Find in Mountain.java
@@ -0,0 +1,90 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        
+        int PeakElementIndex = searchPeakElementBS(mountainArr);
+        if( mountainArr.get(PeakElementIndex) == target)
+        {
+            return PeakElementIndex;
+        }
+        
+        // Find in ascending part of array
+        int ans = -1;
+        ans = OrderAgnosticBinarySearch(mountainArr, 0, PeakElementIndex-1,target);
+        if(ans == -1)
+         {
+            ans = OrderAgnosticBinarySearch(mountainArr, PeakElementIndex+1, mountainArr.length()-1,target);
+            return ans;
+         }
+        return ans;
+        
+        
+             //return PeakElementIndex;
+    }
+    
+    public int searchPeakElementBS(MountainArray mountainArr)
+    {
+        int start =0;
+        int end = mountainArr.length()-1;
+        
+        while(start < end)
+        {
+            int mid = start + (end- start)/2;
+            if(mountainArr.get(mid) > mountainArr.get(mid+1)  && mountainArr.get(mid) > mountainArr.get(mid-1))
+                return mid;
+            
+            if(mid +1 < mountainArr.length() && mountainArr.get(mid) < mountainArr.get(mid+1) ) // Asscending part of array
+            {
+                start = mid+1;
+            }
+            else if (mid +1 < mountainArr.length() && mountainArr.get(mid) > mountainArr.get(mid+1) )  // Descending Part of Array
+                end = mid;
+        }
+        
+        return start ;// or return end
+    }
+    
+    public int OrderAgnosticBinarySearch(MountainArray mountainArr, int start, int end, int target)
+    {   
+        boolean isAscending = mountainArr.get(start) < mountainArr.get(end); 
+        
+        int mid =-1;
+        while(start <= end)
+        {
+            mid = start + (end-start)/2;
+            if(mountainArr.get(mid) == target)
+            {
+                return mid;
+            }
+            if(isAscending)
+            {    
+                if(mountainArr.get(mid) > target)
+                {
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            else{
+                
+                if(mountainArr.get(mid) > target)
+                {
+                    start = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
